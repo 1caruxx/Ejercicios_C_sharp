6e508002cfe4e58f2b0545d6e9250f54c302ef0d
1caruxx <fernandohugolareu@hotmail.com>
2017-04-15 21:46:31 -0300

    gggg
    ggg

---
+++ Ejercicio 25 (solucion)/Ejercicio 25/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. #25";
+
+            int[] vector;
+            string entrada;
+
+            vector = new int[5];
+
+            for(int contador=0 ; contador<5 ; contador++)
+            {
+                Console.WriteLine("Ingrese un numero (tiene que ser != 0)[{0}/5]",contador+1);
+                entrada = Console.ReadLine();
+
+                while(entrada == "0")
+                {
+                    Console.WriteLine("Error, reingre...");
+                    entrada = Console.ReadLine();
+                }
+
+                int.TryParse(entrada , out vector[contador]);
+            }
+
+            Vector.mostrar(vector, 5);
+            Console.Write("\n");
+            Vector.mostrarPositivos(vector , 5);
+            Console.Write("\n");
+            Vector.mostrarNegativos(vector , 5);
+            Console.Write("\n");
+
+            Console.WriteLine("Presione una tecla para finalizar...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio 25 (solucion)/Ejercicio 25/Vector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_25
+{
+    class Vector
+    {
+        public static void mostrar(int[] vector , int cantidad)
+        {
+            for(int contador=0 ; contador<cantidad ; contador++)
+            {
+                Console.Write(vector[contador] + " ");
+            }
+        }
+
+        public static void mostrarPositivos(int[] vector, int cantidad)
+        {
+            int auxiliar;
+
+            for (int contador=0 ; contador<cantidad-1 ; contador++)
+            {
+                for(int subContador=contador+1 ; subContador<cantidad ; subContador++)
+                {
+                    if(vector[contador] > vector[subContador])
+                    {
+                        auxiliar = vector[contador];
+                        vector[contador] = vector[subContador];
+                        vector[subContador] = auxiliar;
+                    }
+                }
+            }
+            for (int contador = 0; contador < cantidad; contador++)
+            {
+                if(vector[contador]>0)
+                {
+                    Console.Write(vector[contador] + " ");
+                }
+            }
+        }
+
+        public static void mostrarNegativos(int[] vector, int cantidad)
+        {
+            int auxiliar;
+
+            for (int contador = 0; contador < cantidad - 1; contador++)
+            {
+                for (int subContador = contador + 1; subContador < cantidad; subContador++)
+                {
+                    if (vector[contador] < vector[subContador])
+                    {
+                        auxiliar = vector[contador];
+                        vector[contador] = vector[subContador];
+                        vector[subContador] = auxiliar;
+                    }
+                }
+            }
+            for (int contador = 0; contador < cantidad; contador++)
+            {
+                if (vector[contador] < 0)
+                {
+                    Console.Write(vector[contador] + " ");
+                }
+            }
+        }
+    }
+}
